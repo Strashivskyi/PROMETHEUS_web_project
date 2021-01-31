92612ae30787376b378eea5db59985c314042f52
Maria-Sparrow <53764665+Maria-Sparrow@users.noreply.github.com>
2021-01-31 15:33:44 +0200

    Added "ForgetPassword" file with css

--- src/Components/SignIn/ForgetPassword.js
+++ src/Components/SignIn/ForgetPassword.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useCallback, useContext } from 'react'
+import { withRouter, Redirect } from 'react-router'
+import app from '../../Firebase/firebase'
+import '../Login/Login1.css'
+import './ForgetPassword.css'
 
 function ForgetPassword() {
     return (
         <>
-            <br />
-            <br />
-            <br />
-            <p>HERE YOU CAN CHANGE YOUR PASSWORD</p>
+            <div className="image-form-division">
+                <div className="image" />
+                <div className="form">
+                    <div className="sign-up-text"> Відновлення паролю</div>
+                    <form className="forget_form" >
+                        <div>**Якщо Ви забули свій пароль, будь ласка,
+                             введіть поточну електронну адресу і перевірте свою пошту. 
+                             Вам має прийти лист з посиланням,за яким Ви повинні перейти і 
+                             ввести новий пароль.</div>
+                        <input
+                            className="forget_text_input"
+                            type="text"
+                            id="fname"
+                            name="email"
+                            placeholder="Email"
+                        />
+                        <input
+                            className="forget_submit"
+                            type="submit"
+                            value="Відправити"
+                        />
+                    </form>
+                </div>
+            </div>
         </>
     )
+
 }
 export default ForgetPassword
