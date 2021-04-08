4e90b5fa3c7d6cab74583e9dc64e2c07e7b462e0
svyat <svyatoslavtrostynski24@gmail.com>
2021-04-08 10:09:25 +0300

    (Maxym) updated SuperVisorTherapist Nav

--- src/Components/Patient/SupervisorTherapistNav.js
+++ src/Components/Patient/SupervisorTherapistNav.js
@@ -1,25 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './SupervisorTherapistNav.css'
 
 export default function SupervisorTherapistNav() {
     return (
         <>
-            <div className="col-12 col-md-6 row justify-content-between">
+            <div className="col-6 row justify-content-between">
                 <span
-                    onClick={() => (
-                        localStorage.setItem('proffesion', 'Supervisors'),
-                        window.location.reload()
-                    )}
-                    className="role_choice_link col-6 col-md-4 supervisor_choice_link"
+                         onClick={()=>(localStorage.setItem("proffesion","Supervisors"),window.location.reload())}
+                  
+                    className="role_choice_link supervisor_choice_link col-4 text-align-right"
                 >
                     Супервізор
                 </span>
                 <span
-                    onClick={() => (
-                        localStorage.setItem('proffesion', 'Therapists'),
-                        window.location.reload()
-                    )}
-                    className="role_choice_link col-6 col-md-4 therapist_choice_link"
+                 
+                 onClick={()=>(localStorage.setItem("proffesion","Therapists"),window.location.reload())}
+                    className="role_choice_link col-4 text-align-center"
                 >
                     Терапіст
                 </span>
