6e791b77b0b4f39279b6dea2516e9c6d1918ad7d
svyat <svyatoslavtrostynski24@gmail.com>
2021-05-18 10:27:37 +0200

    (max&me) updated TherapyResult

--- src/Components/TherapyResult/TherapyResult.js
+++ src/Components/TherapyResult/TherapyResult.js
@@ -6,55 +6,14 @@ import {Link} from 'react-router-dom'
 import {withRouter, Redirect} from 'react-router'
 import PaginationRounded from './Pagination'
 import MobileHeader from '../Header/MobileHeader'
+import TreeArrowHeader from '../ArrowHeader/TreeArrowHeader'
 function TherapyResult({history}) {
     return (
         <>
             <MobileHeader />
             <Header />
-            <div
-                className="patient_upper_flex_container"
-                style={{marginTop: '10px'}}
-            >
-                <h2 style={{marginLeft: '100px'}}>
-                    <Link to="/" className="patients_link">
-                        Пацієнти
-                    </Link>
-                </h2>
-                <img
-                    src={Arrow}
-                    height="20"
-                    style={{
-                        marginLeft: '30px',
-                        marginRight: '30px',
-                        marginTop: '28px',
-                        color: 'black',
-                    }}
-                />
-                <h2>
-                    <Link to="/patient" className="patients_link">
-                        {localStorage.getItem('childName')}
-                    </Link>
-                </h2>
-                <img
-                    src={Arrow}
-                    height="20"
-                    style={{
-                        marginLeft: '30px',
-                        marginRight: '30px',
-                        marginTop: '28px',
-                        color: 'black',
-                    }}
-                />
-                <h2>
-                    <Link
-                        style={{color: '#666666', cursor: 'default'}}
-                        className="patients_link"
-                        to="/protocol-list"
-                    >
-                        Результати терапії
-                    </Link>
-                </h2>
-            </div>
+            
+          <TreeArrowHeader patient={localStorage.getItem("childName")} page={"Результати терапії"}/>
             <div className="container_file">
                 <p class="choose-file-title">
                     Натисніть на файл, щоб завантажити його
