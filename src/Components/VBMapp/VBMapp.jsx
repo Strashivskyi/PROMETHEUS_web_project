9934e07b6d7ae960acb71a8f3d4b106a683e3077
aekrops <svyatoslavtrostynski24@gmail.com>
2021-07-01 02:15:20 +0300

    update VBMap

--- src/Components/VBMapp/VBMapp.jsx
+++ src/Components/VBMapp/VBMapp.jsx
@@ -1,18 +1,18 @@
 import app from '../../Firebase/firebase'
-import "./VBMapp.css"
-import { Link } from "react-router-dom"
-import React, { useEffect, useState } from 'react'
+import './VBMapp.css'
+import {Link} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
 import HeaderHomeTemplate from '../Header/HeaderHomeTemplate'
 import Stage from './ProgramElement/Stage'
-import moment from "moment";
+import moment from 'moment'
 function VBMapp() {
     const [protocols, setProtocols] = useState([])
     useEffect(() => {
         const db = app.firestore()
         const unsubscribe = db
-        .collection("Users")
-        .doc(localStorage.getItem('user'))
-        .collection(localStorage.getItem('proffesion'))
+            .collection('Users')
+            .doc(localStorage.getItem('user'))
+            .collection(localStorage.getItem('proffesion'))
             .doc(localStorage.getItem('child'))
             .collection('Protocols')
             .onSnapshot((snapshot) => {
@@ -38,12 +38,12 @@ function VBMapp() {
         // /VB-MAPP_protocol/Манд/protocol
         const db = app.firestore()
         const unsubscribe = db
-            .collection("Users")
+            .collection('Users')
             .doc(localStorage.getItem('user'))
             .collection(localStorage.getItem('proffesion'))
             .doc(localStorage.getItem('child'))
-            .collection("VB-MAPP_protocol")
-            .orderBy("prior", "asc")
+            .collection('VB-MAPP_protocol')
+            .orderBy('prior', 'asc')
             .onSnapshot((snapshot) => {
                 if (snapshot.size) {
                     setProgram(
@@ -61,53 +61,70 @@ function VBMapp() {
             unsubscribe()
         }
     }, [])
-    return (<>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-            <HeaderHomeTemplate />
-            <div className="program-main-block" style={{ overflowY: 'scroll' }}>
-                {program.map((elementProgram) => (
-                    <div style={{ display: "flex", flexDirection: "column", margin: "0% 2%" }}>
-                        <span className="program-element" style={{ margin: "25% 0 " }}>{elementProgram.id}</span>
-                        <Stage id={elementProgram.id} />
-
-                    </div>
-                ))}
-            </div>
-            <Link style={{ textDecoration: "none", alignSelf: "center" }} to="/protocol-list" onClick={()=> MakeHistory(protocols)}>
+    return (
+        <>
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <HeaderHomeTemplate />
                 <div
-                    class="go-to-program"
-
+                    className="program-main-block"
+                    style={{overflowY: 'scroll'}}
                 >
-
-                    <span class="go-to-program-text">Перейти до програми</span>
-
+                    {program.map((elementProgram) => (
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                margin: '0% 2%',
+                            }}
+                        >
+                            <span
+                                className="program-element"
+                                style={{margin: '25% 0 '}}
+                            >
+                                {elementProgram.id}
+                            </span>
+                            <Stage id={elementProgram.id} />
+                            <br />
+                        </div>
+                    ))}
                 </div>
-            </Link>
-
-        </div>
-
-
-    </>)
+                <br />
+                <Link
+                    style={{textDecoration: 'none', alignSelf: 'center'}}
+                    to="/protocol-list"
+                    onClick={() => MakeHistory(protocols)}
+                >
+                    <div class="go-to-program">
+                        <span class="go-to-program-text">
+                            Перейти до програми
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </>
+    )
 }
 export default VBMapp
 
 function MakeHistory(protocols) {
-let date=moment().format("DD-MM-YYYY HH:mm:ss")
+    let date = moment().format('DD-MM-YYYY HH:mm:ss')
     const db = app.firestore()
-    db.collection("Users")
+    db.collection('Users')
         .doc(localStorage.getItem('user'))
-        .collection("Supervisors")
+        .collection('Supervisors')
         .doc(localStorage.getItem('child'))
-        .collection("History")
-        .doc(date).set({Date:date})
-    protocols.map((protocol)=>
-    db.collection("Users")
-        .doc(localStorage.getItem('user'))
-        .collection("Supervisors")
-        .doc(localStorage.getItem('child'))
-        .collection("History")
+        .collection('History')
         .doc(date)
-        .collection("protocols")
-        .add(protocol)
-        )
+        .set({Date: date})
+    protocols.map((protocol) =>
+        db
+            .collection('Users')
+            .doc(localStorage.getItem('user'))
+            .collection('Supervisors')
+            .doc(localStorage.getItem('child'))
+            .collection('History')
+            .doc(date)
+            .collection('protocols')
+            .add(protocol)
+    )
 }
