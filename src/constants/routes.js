d18478966d5644c729d5d1cea0e53f8fbc25fcb5
aekrops <svyatoslavtrostynski24@gmail.com>
2021-07-01 03:10:23 +0300

    added routes constant

---
+++ src/constants/routes.js
@@ -0,0 +1,32 @@
+//usage -> import * as ROUTES from '../../constants/routes'; <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+export const LANDING = ''
+export const SIGN_UP = '/registration'
+export const SIGN_IN = '/'
+export const PASSWORD_FORGET = '/forgetPassword'
+export const PATIENTS = '/home'
+export const PATIENT = '/patient'
+export const PROTOCOLS = '/protocol-list'
+export const PROGRAM = '/program'
+export const REGISTRATION_PATIENT = '/patient_registration'
+export const THERAPY_RESULT = '/patient/result_of_therapy'
+export const PROGRAM_HISTORY = '/program-template'
+export const VBMAPP = '/vb-mapp'
+export const PUBLIC_TEMPLATES = '/public-template'
+
+//     path="/history-protocol-list"
+//     component={HistoryProtocolList}
+
+//     path="/program-template"
+//     component={ProgramTemplate}
+
+//     path="/create-template"
+//     component={ProtocolListTemplate}
+
+//     path="/program-template-edit"
+//     component={ProgramEditableTemplate}
+
+//     path="/template-more-info"
+//     component={ListMoreInfoTemplate}
+
+//     path="/template-more-info-protocol"
+//     component={ProgramTemplateView}
